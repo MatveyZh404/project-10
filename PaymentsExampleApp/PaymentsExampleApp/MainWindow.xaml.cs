ee02b1fbd55ded32ff8bbb1505835d5f0ccf0521
MatveyZh404 <118770204+MatveyZh404@users.noreply.github.com>
2023-05-03 10:11:35 +0300

    Add files via upload

--- PaymentsExampleApp/PaymentsExampleApp/MainWindow.xaml.cs
+++ PaymentsExampleApp/PaymentsExampleApp/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +21,26 @@ namespace PaymentsExampleApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private PaymentsBaseEntities _context = new PaymentsBaseEntities();
         public MainWindow()
         {
             InitializeComponent();
+            ChartPayments.ChartAreas.Add(new ChartArea("Main"));
+
+            var currentSeries = new Series("Payments");
+            {
+                IsValueShownAsLabel = true;
+            }
+            ChartPayments.Series.Add(currentSeries);
+
+            ComboUsers.IteamsSource = _context.Users.ToList();
+            ComboChartTypes.ItemsSource = Enum.GetValues(typeof(SeriesChartType));
         }
+    }
+    private void UpdateChart(object sender, SelectionChangedEventArgs e)
+    {
+        if (ComboUsers.SelectedIteam is User currentUser &&
+                ComboChartTypes.SelectedItem is SeriesChartType currentType)
+
     }
 }
